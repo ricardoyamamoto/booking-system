643345d6a1ac95155078988075212d637602942a
ricardoyamamoto <ricardo.yamamoto@gmail.com>
2017-05-29 20:54:36 -0400

    Test
    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Booking System';
+  courses: Course[];
+  constructor() {
+    this.courses = [
+      {
+      'academy': 'EASY',
+      'year': 2017,
+      'title': 'ITO',
+      'education': 'Computer Science',
+      'lecturer': 'Lars Bilde'
+      },
+      {
+        'academy': 'EASY',
+        'year': 2017,
+        'title': 'Test',
+        'education': 'Computer Science',
+        'lecturer': 'Lars Bilde'
+      }
+    ];
+  }
+
 }
